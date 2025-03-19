2402ddbcf724922a0e3d3b180888df06bd2e0f3f
Maxim Zharinoff <minimum.cold1991@gmail.com>
2025-03-19 10:00:56 +0700

    create SecurityConfig

---
+++ src/main/java/ru/netology/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package ru.netology.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/public").permitAll()
+                .antMatchers("/admins").hasAuthority("admin")
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .authorities("admin")
+                .and()
+                .withUser("user")
+                .password(passwordEncoder().encode("user"))
+                .authorities("user");
+    }
+}
